20566da5e50c8a9ea4fc06cfc9427c94913ff9b5
Sethum3001 <sethumrashmika3001@gmail.com>
2021-08-08 19:53:35 +0530

    1

---
+++ src/StudentFormController.java
@@ -0,0 +1,28 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StudentFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public TextField txtSearch;
+    public TableView tblStudents;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+
+    public void btnSaveStudentOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnUpdateStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteStudentOnAction(ActionEvent actionEvent) {
+
+    }
+}
